8c7a71cc4c185797d33ec58b9a639fc262533e7d
이예지 <oyatpeach@naver.com>
2024-07-19 15:59:50 +0900

    Design : 옷장 정리 현황, 상품 종합 현황 topinfo design

--- src/app/totalItem/page.tsx
+++ src/app/totalItem/page.tsx
@@ -1,10 +1,15 @@
 import MainTitle from "@/components/titles/MainTitle";
+import TopInfo from "@/components/wardrobe/TopInfo";
+const total = 12;
 
 function item() {
   return (
     <div className="mt-69pxr ml-104pxr">
       <div>
         <MainTitle mainTitleName="상품 종합 현황" />
+        <div>
+          <TopInfo total={total} />
+        </div>
       </div>
     </div>
   );
